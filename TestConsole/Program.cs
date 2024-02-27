d5fec60b8f3cc981da51b58ca4cfd41134fa0bf0
NihadRasulzada <nihadrs2005@gmail.com>
2024-02-27 22:49:09 +0400

    Initialize

---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestConsole
+{
+    public class Program
+    {
+        static void Main()
+        {
+            double x = 0.1, eps = 0.01, z1 = 1, S = 0;
+            int fact = 1, i = 1;
+            while (Math.Abs(z1) > eps)
+            {
+                z1 = Math.Pow(x, i);
+                fact *= i++;
+                z1 /= fact;
+                S += z1;
+            }
+            Console.WriteLine(++S);
+        }
+    }
+}
